a089dbd7e3289bb85d3a3defb241328a8c219aaa
AKHIL-DyC <119416316+AKHIL-DyC@users.noreply.github.com>
2023-10-05 11:46:22 +0530

    Add files via upload
    address

---
+++ 2-address/Address.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Address
+{
+public static void main(String args[])
+{
+System.out.println("Name:\tAKHIL");
+System.out.println("age:\t20");
+System.out.println("address:mammante veed\n\tkarimmakad\n\tkochi");
+
+
+}
+}
