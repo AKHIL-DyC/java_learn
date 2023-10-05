131d40ae5b045083ab1c707303e29fbd4a3d7721
AKHIL-DyC <119416316+AKHIL-DyC@users.noreply.github.com>
2023-10-05 11:46:45 +0530

    Add files via upload
    even-odd

---
+++ 3-even-odd/Eo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Eo
+{
+public static void main(String args[])
+
+{int n=9;
+if(n%2==0){
+	System.out.println("even number");
+}
+else{System.out.println("odd number");}
+}
+}
