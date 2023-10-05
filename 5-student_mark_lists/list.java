380d3a2b0b599c745e7c705c98215d468532b17d
AKHIL-DyC <119416316+AKHIL-DyC@users.noreply.github.com>
2023-10-05 11:47:27 +0530

    student mark list

---
+++ 5-student_mark_lists/list.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ class List {
+	public static void main(String args[])
+	{
+		System.out.println("akhil");
+		System.out.println("java\t"+80);
+		System.out.println("maths\t"+60);
+		System.out.println("cp\t"+90);
+		System.out.println("ds\t"+70);
+		System.out.println("lsd\t"+100);
+		
+	}
+}
